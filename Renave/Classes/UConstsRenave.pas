911b4ba3fe85ffc3cc416f73bc4fd5b6e6e9d78d
Aggille Sistemas <aggille@aggille.com.br>
2021-12-23 16:27:12 -0300

    -

---
+++ Renave/Classes/UConstsRenave.pas
@@ -0,0 +1,35 @@
+unit UConstsRenave;
+
+interface
+
+resourcestring
+  StrInformeMetodoConsulta = 'Informe o método da consulta ( GET / POST )';
+  StrInformeAURL = 'Informe a URL';
+  StrApplicationjson = 'application/json';
+  StrHttpPOST = 'POST';
+  StrHttpGET = 'GET';
+  StrCodigoDoErro = 'Código do erro: ';
+  StrErroInesperado = 'Erro inesperado';
+  StrErroNaoAutorizado = 'Não autorizado';
+  StrErroAcessoNegado = 'Acesso negado';
+  StrErroServicoNaoEncontrado = 'Serviço não encontrado';
+  StrErroConsulta = 'ERRO NA CONSULTA';
+  StrTituloErro = 'Título : ';
+  StrDetalheErro = 'Detalhe : ';
+  StrMensagemErro = 'Mensagem : ';
+  StrVeiculosDisponiveis = 'VEÍCULOS DISPONÍVEIS';
+
+const
+  IntHttpCode200 = 200;
+  IntHttpCode201 = 201;
+  IntHttpCode401 = 401;
+  IntHttpCode403 = 403;
+  IntHttpCode404 = 404;
+  IntHttpCode422 = 422;
+  IntHttpCode500 = 500;
+
+
+implementation
+
+
+end.
